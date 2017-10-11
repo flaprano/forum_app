e345395d5241869e9e9df6230bb8269b1043ed48
Felipe Laprano <flaprano@gmail.com>
2017-10-11 11:44:36 +0000

    Adding seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,18 +17,5 @@ regex = ProfanityWord.generate_regex word
 ProfanityWord.create(word: word, regex: regex)
 
 20.times do |i|
-  topic = Post.create(title: "Tópico #{i}", message: "Mensagem do tópico #{i}")
-  #topic.children.create(title: topic.title, message: "Comentário do tópico #{i}. Podemos comprar um cav@lo.")
-  #topic.children.create(title: topic.title, message: "Comentário do tópico #{i}. Podemos comprar um baralho e talvez uma f@ac@.")
-  #topic.children.create(title: topic.title, message: "Comentário do tópico #{i}. Podemos comprar uma faca.") 
-  #topic.children.create(title: topic.title, message: "Comentário do tópico #{i}. Podemos comprar uma @rm@.")
+  Post.create(title: "Tópico #{i}", message: "Mensagem do tópico #{i}")
 end
-
-topic = Post.all[0]
-
-#topic.children.each do |comment|
-#  comment.children.create(title: comment.title, message: "Resposta do comentário. Podemos comprar um cav@lo.")
-#  comment.children.create(title: comment.title, message: "Resposta do comentário. Podemos comprar um baralho e talvez uma f@ac@.")
-#  comment.children.create(title: comment.title, message: "Resposta do comentário. Podemos comprar uma faca.") 
-#  comment.children.create(title: comment.title, message: "Resposta do comentário. Podemos comprar uma @rm@.")
-#end
